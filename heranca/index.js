3e577cc7f76cda52878637ea75c22f829f7d6269
igor gomes <gomesoliveirasl02@hotmail.com>
2024-01-11 20:35:05 -0300

    heranca de class

---
+++ heranca/index.js
@@ -0,0 +1,29 @@
+class DispositivoEletronico{
+    constructor(nome){
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar(){
+        if(this.ligado){
+            console.log(this.nome + ' ja ligado')
+            return
+        }
+        this.ligado = true;
+    }
+
+    desligar(){
+        if(!this.ligado){
+            console.log(this.nome + ' ja desligado')
+            return
+        }
+    }
+}
+
+const d1 = new DispositivoEletronico('iphone');
+d1.ligar();
+console.log(d1)
+
+class Smartphone extends DispositivoEletronico{}
+const s1 = new Smartphone('iphone');
+s1.ligar();
